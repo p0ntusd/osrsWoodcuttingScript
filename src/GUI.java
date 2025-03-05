4e5f7bb0f9ee18a82246c7dc8401b7ff21d12f55
pontus <dahlkvistpontus@gmail.com>
2025-03-05 21:47:06 +0100

    gui

--- src/GUI.java
+++ src/GUI.java
@@ -3,8 +3,6 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.Arrays;
 
-/*This class creates a Graphical User Interface for the user to initialize some variables needed to
- * start the bot. It sends the information to Fisher and then closes the GUI*/
 public class GUI extends JFrame implements ActionListener {
     private Container container;
     private JLabel title;
@@ -21,7 +19,6 @@ public class GUI extends JFrame implements ActionListener {
     private JButton submitButton;
     private JRadioButton fastRadioButton, mediumRadioButton, slowRadioButton;
     private ButtonGroup speedButtonGroup;
-    private final String[] FISH_OPTIONS = {"Small Net", "Bait", "Lure", "Harpoon", "Use-rod"};
     private JLabel baitIDLabel;
     private JTextField baitIDTextField;
 
@@ -33,24 +30,24 @@ public class GUI extends JFrame implements ActionListener {
         container = getContentPane();
         container.setLayout(null);
 
-        title = new JLabel("Welcome to Fisher!");
+        title = new JLabel("Woodcutter 1.0");
         title.setFont(new Font("Arial", Font.PLAIN, 30));
         title.setSize(400, 50);
         title.setLocation(150, 30);
         container.add(title);
 
         //fishing type objects
-        fishingTypeLabel = new JLabel("Select Type of Fishing:");
-        fishingTypeLabel.setFont(new Font("Arial", Font.PLAIN, 20));
-        fishingTypeLabel.setSize(250, 30);
-        fishingTypeLabel.setLocation(50, 100);
-        container.add(fishingTypeLabel);
+//        fishingTypeLabel = new JLabel("Select Type of Fishing:");
+//        fishingTypeLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+//        fishingTypeLabel.setSize(250, 30);
+//        fishingTypeLabel.setLocation(50, 100);
+//        container.add(fishingTypeLabel);
 
-        fishingTypeCombo = new JComboBox<>(FISH_OPTIONS);
-        fishingTypeCombo.setFont(new Font("Arial", Font.PLAIN, 15));
-        fishingTypeCombo.setSize(350, 30);
-        fishingTypeCombo.setLocation(300, 100);
-        container.add(fishingTypeCombo);
+//        fishingTypeCombo = new JComboBox<>(FISH_OPTIONS);
+//        fishingTypeCombo.setFont(new Font("Arial", Font.PLAIN, 15));
+//        fishingTypeCombo.setSize(350, 30);
+//        fishingTypeCombo.setLocation(300, 100);
+//        container.add(fishingTypeCombo);
 
         //inventory objects
         inventoryToKeepLabel = new JLabel("Items to keep in inventory: ");
@@ -66,7 +63,7 @@ public class GUI extends JFrame implements ActionListener {
         container.add(inventoryToKeepTextField);
 
         //fishing spot objects
-        fishingSpotLabel = new JLabel("Enter Fishing Spot IDs:");
+        fishingSpotLabel = new JLabel("Enter TREE IDS:");
         fishingSpotLabel.setFont(new Font("Arial", Font.PLAIN, 20));
         fishingSpotLabel.setSize(250, 30);
         fishingSpotLabel.setLocation(50, 200);
@@ -79,7 +76,7 @@ public class GUI extends JFrame implements ActionListener {
         container.add(fishingSpotTextfField);
 
         //fish caught objects
-        fishgCaughtLabel = new JLabel("Fish Caught ID:");
+        fishgCaughtLabel = new JLabel("Logs cut ID:");
         fishgCaughtLabel.setFont(new Font("Arial", Font.PLAIN, 20));
         fishgCaughtLabel.setSize(250, 30);
         fishgCaughtLabel.setLocation(50, 250);
@@ -93,7 +90,7 @@ public class GUI extends JFrame implements ActionListener {
 
 
         //Fishing speed objects (radio buttons)
-        fishingSpeedLabel = new JLabel("Fishing Speed:");
+        fishingSpeedLabel = new JLabel("Woodcutting Speed:");
         fishingSpeedLabel.setFont(new Font("Arial", Font.PLAIN, 20));
         fishingSpeedLabel.setSize(200, 30);
         fishingSpeedLabel.setLocation(50, 300);
@@ -102,21 +99,21 @@ public class GUI extends JFrame implements ActionListener {
         slowRadioButton = new JRadioButton("Slow");
         slowRadioButton.setFont(new Font("Arial", Font.PLAIN, 15));
         slowRadioButton.setSize(70, 30);
-        slowRadioButton.setLocation(200, 300);
+        slowRadioButton.setLocation(270, 300);
         slowRadioButton.setSelected(false);
         container.add(slowRadioButton);
 
         fastRadioButton = new JRadioButton("Medium");
         fastRadioButton.setFont(new Font("Arial", Font.PLAIN, 15));
         fastRadioButton.setSize(80, 30);
-        fastRadioButton.setLocation(270, 300);
+        fastRadioButton.setLocation(360, 300);
         fastRadioButton.setSelected(false);
         container.add(fastRadioButton);
 
         mediumRadioButton = new JRadioButton("Fast", true);
         mediumRadioButton.setFont(new Font("Arial", Font.PLAIN, 15));
         mediumRadioButton.setSize(80, 30);
-        mediumRadioButton.setLocation(360, 300);
+        mediumRadioButton.setLocation(500, 300);
         mediumRadioButton.setSelected(true);
         container.add(mediumRadioButton);
 
@@ -125,12 +122,12 @@ public class GUI extends JFrame implements ActionListener {
         speedButtonGroup.add(mediumRadioButton);
         speedButtonGroup.add(fastRadioButton);
 
-        //bait id objects
-        baitIDLabel = new JLabel("Bait ID:");
-        baitIDLabel.setFont(new Font("Arial", Font.PLAIN, 20));
-        baitIDLabel.setSize(200, 30);
-        baitIDLabel.setLocation(50, 350);
-        container.add(baitIDLabel);
+//        //bait id objects
+//        baitIDLabel = new JLabel("Bait ID:");
+//        baitIDLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+//        baitIDLabel.setSize(200, 30);
+//        baitIDLabel.setLocation(50, 350);
+//        container.add(baitIDLabel);
 
         baitIDTextField = new JTextField();
         baitIDTextField.setFont(new Font("Arial", Font.PLAIN, 15));
@@ -148,17 +145,17 @@ public class GUI extends JFrame implements ActionListener {
         container.add(submitButton);
 
         //setting the baitID to editable if appropriate
-        fishingTypeCombo.addItemListener(new ItemListener() {
-            @Override
-            public void itemStateChanged(ItemEvent e) {
-                if(fishingTypeCombo.getSelectedItem().toString().equals("Bait") ||
-                        fishingTypeCombo.getSelectedItem().toString().equals("Lure") ||
-                        fishingTypeCombo.getSelectedItem().toString().equals("Use-rod"))
-                    baitIDTextField.setEditable(true);
-                else
-                    baitIDTextField.setEditable(false);
-            }
-        });
+//        fishingTypeCombo.addItemListener(new ItemListener() {
+//            @Override
+//            public void itemStateChanged(ItemEvent e) {
+//                if(fishingTypeCombo.getSelectedItem().toString().equals("Bait") ||
+//                        fishingTypeCombo.getSelectedItem().toString().equals("Lure") ||
+//                        fishingTypeCombo.getSelectedItem().toString().equals("Use-rod"))
+//                    baitIDTextField.setEditable(true);
+//                else
+//                    baitIDTextField.setEditable(false);
+//            }
+//        });
         setVisible(true);
     }
     @Override
@@ -166,7 +163,7 @@ public class GUI extends JFrame implements ActionListener {
         if(e.getSource() == submitButton) {
             try {
                 //initializes Fishing type, inventory to keep, fishing spots and fish caught (to drop)
-                Woodcutter.WOODCUTTING_TYPE = fishingTypeCombo.getSelectedItem().toString();
+//                Woodcutter.WOODCUTTING_TYPE = fishingTypeCombo.getSelectedItem().toString();
                 Woodcutter.INVENTORY_TO_KEEP = Arrays.stream(inventoryToKeepTextField.getText().split(",")).mapToInt(Integer::parseInt).toArray();
                 Woodcutter.TREE_IDS = Arrays.stream(fishingSpotTextfField.getText().split(",")).mapToInt(Integer::parseInt).toArray();
                 Woodcutter.WOOD_CUT_IDS = Arrays.stream(fishCaughtTextfField.getText().split(",")).mapToInt(Integer::parseInt).toArray();
